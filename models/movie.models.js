a13e9386d31f0cc651a2c03ebf8e1f6c885cfb37
Siddhant Prateek <siddhantprateek@gmail.com>
2022-07-03 13:04:09 +0530

    users auth and movie schema

---
+++ models/movie.models.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const movieSchema = new mongoose.Schema({
+    original_title: String,
+    overview: String,
+    poster_path: String,
+    movie_type: [String],
+    release_date: String,
+    rating: Number,
+    reviews_count: Number
+})
+
+module.exports = mongoose.model('movierealmdb', movieSchema)
+
