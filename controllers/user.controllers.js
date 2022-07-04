9f9377d9b559729785ba4e5665c24a0a6870557b
Siddhant Prateek <siddhantprateek@gmail.com>
2022-07-05 01:42:41 +0530

    user controllers

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,5 +1,5 @@
 const UserModel = require('../models/users.models')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const generateToken = (id) => {
